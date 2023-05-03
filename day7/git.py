ac0f6b1a060df0bcd150fa0078b817013e8c9d36
pnabiniw <pnabiniw@gmail.com>
2023-05-03 09:23:48 +0545

    addition

--- day7/git.py
+++ day7/git.py
@@ -1,6 +1,8 @@
 # git init
 # git add .  (git add --all)
 # git commit -m "<commit_message>"
+# git remote add origin <ssh link>
+# git push -u origin <branch_name>
 
 # git status  # Shows the current status of all git staged and
 # unstaged file
@@ -8,3 +10,20 @@
 # Setting git configuration
 # 1. git config --global user.name "<your-username>"
 # 2. git config --global user.email "<your-email"
+
+
+# Generating ssh key pair
+# ssh-keygen -t rsa -b 4096 -C "your_email@example.com"
+
+
+##### Git Branching ######3
+
+# git branch  # It shows all the branches and highlights the
+# current branch
+
+# git branch <branch_name>   # Creates a new branch
+# git checkout <branch_name>  # jumps (checkout) to the next branch
+
+# git checkout -b <branch_name>  # Creates a new branch and jumps to that
+# branch
+
