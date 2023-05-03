b30fb30c6eec7bbb9e9c86bf211e96094ef4fe57
pnabiniw <pnabiniw@gmail.com>
2023-05-03 08:33:31 +0545

    git commands changes

--- day7/git.py
+++ day7/git.py
@@ -1,2 +1,10 @@
 # git init
-# git add .
+# git add .  (git add --all)
+# git commit -m "<commit_message>"
+
+# git status  # Shows the current status of all git staged and
+# unstaged file
+
+# Setting git configuration
+# 1. git config --global user.name "<your-username>"
+# 2. git config --global user.email "<your-email"
